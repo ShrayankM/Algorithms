47af89feb6a7ebd0deea2985633d8251cdb068cc
ShrayankM <shraymist@gmail.com>
2020-05-21 10:12:33 +0530

    added 21/05/2020

---
+++ B2 - Maths Fundamentals(Udemy)/primeNumber.cpp
@@ -0,0 +1,29 @@
+#include<cstdio>
+#include<math.h>
+using namespace std;
+//O(N) time complexity
+bool isPrime(int N){
+    if(N == 0 || N == 1)
+        return false;
+    for(int i = 2; i < N; i++){
+        if(N % i == 0)
+            return false;
+    }
+    return true;
+}
+//O(sqrt(N)) time complexity
+bool isPrimeSqrt(int N){
+    if(N == 0 || N == 1)
+        return false;
+    for(int i = 2; i < sqrt(N); i++){
+        if(N % i == 0)
+            return false;
+    }
+    return true;
+}
+int main(){
+    int N = 13;
+    printf("%d\n",isPrime(N));
+    printf("%d\n",isPrimeSqrt(N));
+    return 0;
+}
