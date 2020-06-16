e685e7d85ae6b7fa83b2572d9d566a69884be38e
ShrayankM <shraymist@gmail.com>
2020-06-16 09:37:11 +0530

    added 16/06/2020

---
+++ CP - Handbook/bubbleSort.cpp
@@ -0,0 +1,26 @@
+#include<cstdio>
+using namespace std;
+
+void bubbleSort(int A[], int N){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < (N - 1 - i); j++){
+            if(A[j] > A[j + 1]){
+                int T = A[j];
+                A[j] = A[j + 1]; A[j + 1] = T;
+            }
+        }
+    }
+}
+
+void display(int A[], int N){
+    for(int i = 0; i < N; i++)
+        printf("%d ", A[i]);
+    printf("\n");
+}
+int main(){
+    int A[] = {45, 4, 67, 12, 78, 3, 2, 16, 21, 79, 78, 45, 64, 55};
+    int N = 14;
+    bubbleSort(A, N);
+    display(A, N);
+    return 0;
+}
