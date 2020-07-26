0267765008cad4683a05bd478c0028fa816cb5ec
ShrayankM <shraymist@gmail.com>
2020-07-26 10:00:23 +0530

    added 26/07/2020

--- A12 - Stack(Udemy)/infixTopost.cpp
+++ A12 - Stack(Udemy)/infixTopost.cpp
@@ -1,63 +1,63 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int pre(char t){
+int outStack(char t){
     if(t == '+' || t == '-')
         return 1;
     else if(t == '*' || t == '/')
-        return 2;
-    else if(t == '^')
         return 3;
+    else if(t == '^')
+        return 6;
+    else if(t == ')')
+        return 0; //for ( parenthesis
     else
-        return -1;
+        return 7;
+}
+
+int inStack(char t){
+    if(t == '+' || t == '-')
+        return 2;
+    else if(t == '*' || t == '/')
+        return 4;
+    else if(t == '^')
+        return 5;
+    else
+        return 0; //for ( parenthesis
 }
 
 char* findPost(string s){                     //char* findPost(char s[])
-    char* postFix = new char[s.size() + 1];  //char* postFix = new char[strlen(s) + 1];
+    char* postFix = new char[s.size() + 1];
     stack<char>st;
     int k = 0;
     for(int i = 0; s[i] != '\0'; ){
         if(s[i] >= 'a' && s[i] <= 'z')
             postFix[k++] = s[i++];
-        else{
-            if(st.empty() || s[i] == '(')
-                st.push(s[i++]);
-            else if(s[i] == ')'){
-                while(!st.empty() && st.top() != '('){
-                    char temp = st.top();
-                    postFix[k++] = temp;
-                    st.pop();
-                }
-                st.pop();
-                i++;
-            }
-            else if(pre(s[i]) > pre(st.top()))
-                st.push(s[i++]);
-            else if(pre(s[i]) <= pre(st.top())){
-                char temp = st.top();
+        else if(s[i] == '(' || st.empty())
+            st.push(s[i++]);
+        else if(outStack(s[i]) > inStack(st.top()))
+            st.push(s[i++]);
+        else if(outStack(s[i]) < inStack(st.top())){
+            char temp = st.top(); st.pop();
+            postFix[k++] = temp;
+        }
+        else if(s[i] == ')'){
+            while(!st.empty() && (outStack(')') != inStack(st.top()))){
+                char temp = st.top(); st.pop();
                 postFix[k++] = temp;
-                st.pop();
             }
-            else{
-                char temp = st.top();
-                postFix[k++] = temp;
-                st.pop();
-            }
+            st.pop();
+            i++;
         }
     }
     while(!st.empty()){
-        postFix[k++] = st.top();
-        st.pop();
+        postFix[k++] = st.top(); st.pop();
     }
     return postFix;
 }
 int main(){
 
-    //char s[] = "a+b*(c^d-e)^(f+g*h)-i";
-    //char s[] = "a+b*c/d";
-    //string s = "a+b*(c^d-e)^(f+g*h)-i";
     vector<string>s = {"a+b*(c^d-e)^(f+g*h)-i", "a+b*c-d/e*f",
-                       "(a+b*c-d)/(e*f)"};
+                       "(a+b*c-d)/(e*f)", "((a+b)*c)-d^e^f"};
     for(auto str: s)
         printf("%s\n", findPost(str));
     return 0;   
