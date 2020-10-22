1b43fc2c90389be144d0eacd6726328a41ac1ee9
ShrayankM <shraymist@gmail.com>
2020-10-22 11:02:49 +0530

    updatedstorage DS bag

--- Princeton Algorithms Part 2/Week 1 Graphs/Undirected Graphs/src/Bag.java
+++ Princeton Algorithms Part 2/Week 1 Graphs/Undirected Graphs/src/Bag.java
@@ -3,6 +3,7 @@ import java.util.Iterator;
 public class Bag<Item> implements Iterable<Item> {
 
     private Node start;
+    private int cnt = 0;
 
     // Node DS for Linked list implementation
     private class Node {
@@ -22,6 +23,7 @@ public class Bag<Item> implements Iterable<Item> {
 
     // Insert data in Bag (Front insert)
     public void insert(Item value) {
+        cnt++;
         Node n = new Node(value);
         if (start == null) {
             start = n;
@@ -30,6 +32,18 @@ public class Bag<Item> implements Iterable<Item> {
         n.next = start;
         start = n;
     }
+
+    // Update data in Bag
+    public void update(Item value, Item nv) {
+        if (start == null) return;
+//        if (start.data == value) start.data = nv;
+
+        Node p = start;
+        while (p != null && p.data != value) p = p.next;
+        if (p == null) return;
+        p.data = nv;
+    }
+
 
     private class BagIterator implements Iterator<Item> {
 
@@ -45,6 +59,11 @@ public class Bag<Item> implements Iterable<Item> {
             p = p.next;
             return t;
         }
+    }
+
+    // No. of items in bag
+    public int count() {
+        return cnt;
     }
 
     @Override
